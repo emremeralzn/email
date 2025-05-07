867598cc5c95893c7c47ed71a9ac89831ac1b2b3
Emre <sjoxlyses7@gmail.com>
2025-05-07 20:39:30 +0300

    requireandappy

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from email_dto import EmailDto
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
+CORS(app)  # Tüm domainlere CORS izni verir
 
 @app.route("/api/email", methods=["POST"])
 def send_email():
@@ -11,17 +13,17 @@ def send_email():
         data = request.get_json()
         email_data = EmailDto(**data)
 
-        # Email mesajı oluştur
+        # Mail ayarları
         msg = EmailMessage()
         msg["From"] = "pelinsy66@gmail.com"
         msg["To"] = "pelinsy66@gmail.com"
         msg["Subject"] = f"Blog Contact: {email_data.subject}"
         msg.set_content(f"Gönderen: {email_data.to}\n\nMesaj:\n{email_data.body}")
 
-        # Gmail SMTP ile gönder
+        # Gmail SMTP ile gönderim
         with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
             smtp.starttls()
-            smtp.login("pelinsy66@gmail.com", "toaewuhwtouhhwbz")  # Uygulama şifresi
+            smtp.login("pelinsy66@gmail.com", "toaewuhwtouhhwbz")  # Gmail uygulama şifresi
             smtp.send_message(msg)
 
         return jsonify({"message": "Mail başarıyla gönderildi."}), 200
